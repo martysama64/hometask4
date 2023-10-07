38ffd1324728fc8365d00d8650aee40cde486b94
Сагакянц Мартирос <martisagak@gmail.com>
2023-10-07 19:39:54 +0300

    Add the ability to enter language and printing message according to it

--- Desktop/hometask/hello.pas
+++ Desktop/hometask/hello.pas
@@ -1,3 +1,15 @@
 ﻿begin
-  Print('Привет, мир!');
+  var a := ReadString('Enter your language:');
+  case a of
+    'Russian': Print('Привет!');
+    'English': Print('Hello!');
+    'Dutch': Print('Hallo!');
+    'French': Print('Bonjour!');
+    'German': Print('Hallo!');
+    'Danish': Print('Hej!');
+    'Spanish': Print('Hola!');
+  end;
 end.
+{
+Enter your language: Dutch
+Hallo!}
